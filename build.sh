15916eea1be5641a205dbfc8dec71473cbbf5b3f
jdcast <john.d.cast@gmail.com>
2022-09-13 17:05:46 -0400

    Add fix in last commit

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ source config.sh
 echo "Image name is: ${IMAGE_NAME}"
 
 #sudo docker build -t ${IMAGE_NAME} .
-sudo docker build --build-arg APP_PORT="${APP_PORT}" -t "${IMAGE_NAME}" .
+sudo docker build --build-arg APP_PORT=${APP_PORT} -t ${IMAGE_NAME} .
