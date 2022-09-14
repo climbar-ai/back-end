b653835ebbad16e74274ccc57e34c908d3c76a24
jdcast <john.d.cast@gmail.com>
2022-09-14 17:59:25 -0400

    Add server socket restart; Add comments

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+# References:
+# 1. https://stackoverflow.com/questions/46964007/python-socket-connection-between-raspberry-pi-s
+# 2. https://medium.datadriveninvestor.com/connecting-the-microsoft-hololens-and-raspberry-pi3b-58665032964c
+# 3. https://nikhilroxtomar.medium.com/file-transfer-using-tcp-socket-in-python3-idiot-developer-c5cf3899819c
+# 4. https://stackoverflow.com/questions/7185676/send-a-large-file-over-tcp-connection
+
+
 import socket
 import threading
 
@@ -29,14 +36,22 @@ def receiveTCP(sock):
 
 def stopAlarm(sock):
     print("'stop alarm' recieved")
-    sock.send("alarm stopped".encode(FORMAT));
+    sock.send("alarm stopped".encode(FORMAT))
     pass
 
 
 def startAlarm(sock):
     print("'start alarm' recieved")
-    sock.send("alarm started".encode(FORMAT));
+    sock.send("alarm started".encode(FORMAT))
     pass
+
+def closeSock(sock):
+    print("'close sock' recieved")
+    sock.send("closed socket".encode(FORMAT))
+    sock.close()
+
+    # restart server to listen for new connections
+    start_server(IP, PORT)
 
 
 class Waiter(threading.Thread):
@@ -56,7 +71,9 @@ class Waiter(threading.Thread):
                         stopAlarm(sock_server)
                     elif message == 'start alarm':
                         startAlarm(sock_server)
-                    #sock_server.close()
+                    elif message == 'close sock':
+                        closeSock(sock_server)
+                    #sock_server.close() # don't close here for now as this will prevent new messages from same connection coming through
 
 if __name__ == '__main__':  
     ## we expect, as a hand-shake agreement, that there is a .yml config file in top level of lib/configs directory
