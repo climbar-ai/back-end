cf5b10f0930002123301d3bed9c906420f738d74
jdcast <john.d.cast@gmail.com>
2022-09-14 18:13:43 -0400

    Add volume to store hold configurations

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ source config.sh
 # Expose ports and run
 sudo docker run -it \
         -p $PUBLIC_PORT:8081 \
+        -v /home/pi/back-end/hold-configurations:/share/hold-configurations \
         --name $IMAGE_NAME $IMAGE_NAME
