64dd201b50e54cce3c69bb2e8554a6bbcfee6557
jdcast <john.d.cast@gmail.com>
2022-09-14 20:02:21 -0400

    Add single file rx/tx functions

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 # References:
 # 1. https://stackoverflow.com/questions/46964007/python-socket-connection-between-raspberry-pi-s
-# 2. https://medium.datadriveninvestor.com/connecting-the-microsoft-hololens-and-raspberry-pi3b-58665032964c
-# 3. https://nikhilroxtomar.medium.com/file-transfer-using-tcp-socket-in-python3-idiot-developer-c5cf3899819c
-# 4. https://stackoverflow.com/questions/7185676/send-a-large-file-over-tcp-connection
+# 2. https://stackoverflow.com/questions/56194446/send-big-file-over-socket
+# 3. https://stackoverflow.com/questions/55839932/sending-encrypted-data-through-socket-and-decrypting-doesnt-work/55840341#55840341
+# 4. https://medium.datadriveninvestor.com/connecting-the-microsoft-hololens-and-raspberry-pi3b-58665032964c
+# 5. https://nikhilroxtomar.medium.com/file-transfer-using-tcp-socket-in-python3-idiot-developer-c5cf3899819c
 
 
+import os
 import socket
 import threading
 
 IP = '0.0.0.0'
 PORT = 8081
-SIZE = 1024
+BUFFER_SIZE = 1024
 FORMAT = "utf-8"
+HOLD_CONFIGS_DIR = '/share/hold-configurations'
 
 def start_server(host='', port=PORT):
     global sock_server
@@ -30,7 +33,7 @@ def start_server(host='', port=PORT):
 
 
 def receiveTCP(sock):
-    message = sock.recv(SIZE).decode(FORMAT)
+    message = sock.recv(BUFFER_SIZE).decode(FORMAT)
     return message
 
 
@@ -54,6 +57,43 @@ def closeSock(sock):
     start_server(IP, PORT)
 
 
+def receiveFilename(sock):
+    filename = sock.recv(BUFFER_SIZE).decode(FORMAT)
+    print("[RECV] Receiving the filename: {}".format(filename))
+    sock.send("Filename received.".encode(FORMAT))
+    return filename
+
+
+def receiveFile(sock):
+    # receive filename from client
+    filename = receiveFilename(sock)
+
+    # receive file data from client
+    print("[RECV] Receiving the file data.")
+    filepath = os.path.join(HOLD_CONFIGS_DIR, filename)
+    with sock, open(filepath,'wb') as file:
+        while True:
+            recvfile = sock.recv(BUFFER_SIZE).decode(FORMAT)
+            if not recvfile: break
+            file.write(recvfile)
+    print("[RECV] File has been received: {}".format(filename))
+
+
+def sendFile(sock):
+    # receive filename from client
+    filename = receiveFilename(sock)
+
+    # send the filename to client
+    sock.sendall(filename.encode(FORMAT))
+
+    # send the file to client
+    filepath = os.path.join(HOLD_CONFIGS_DIR, filename)
+    with sock, open(filepath,'rb') as f:
+        data = f.read()
+        sock.sendall(data)
+    print("[RECV] File has been sent {}".format(filename))   
+
+
 class Waiter(threading.Thread):
     def __init__(self, **kwargs):
         super(Waiter, self).__init__(**kwargs)
@@ -67,12 +107,16 @@ class Waiter(threading.Thread):
                 pass
             else:
                 if message != '':
-                    if message == 'stop alarm':
+                    if message == 'stopAlarm':
                         stopAlarm(sock_server)
-                    elif message == 'start alarm':
+                    elif message == 'startAlarm':
                         startAlarm(sock_server)
-                    elif message == 'close sock':
+                    elif message == 'closeSock':
                         closeSock(sock_server)
+                    elif message == 'receiveFile':
+                        receiveFile(sock_server)
+                    elif message == 'sendFile':
+                        sendFile(sock_server)
                     #sock_server.close() # don't close here for now as this will prevent new messages from same connection coming through
 
 if __name__ == '__main__':  
