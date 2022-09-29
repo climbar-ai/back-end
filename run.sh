4ce59dffff1d53396b206d0dc0101c1e269401c3
jdcast <john.d.cast@gmail.com>
2022-09-29 13:20:49 -0400

    Change name of volume directory

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ source config.sh
 # Expose ports and run
 sudo docker run -it \
         -p $PUBLIC_PORT:8081 \
-        -v /home/pi/back-end/hold-configurations:/share/hold-configurations \
+        -v /home/pi/back-end/routes:/share/routes \
         --name $IMAGE_NAME $IMAGE_NAME
